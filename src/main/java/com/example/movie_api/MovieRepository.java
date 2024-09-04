994db8f54becef9564d246df6d39c9732a91f6a4
André Mendes Costa <andre.mendes.sp01@gmail.com>
2024-09-04 17:25:31 -0300

    feat: finished movie repository

--- src/main/java/com/example/movie_api/MovieRepository.java
+++ src/main/java/com/example/movie_api/MovieRepository.java
@@ -1,6 +1,8 @@
 package com.example.movie_api;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
     public interface MovieRepository extends JpaRepository <Movie,Long> {
 }
