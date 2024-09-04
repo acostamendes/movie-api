c0487e766c75cd78240be6c1e673ffc68ac94cf7
André Mendes Costa <andre.mendes.sp01@gmail.com>
2024-09-04 17:33:41 -0300

    feat: finished movie Controller

---
+++ src/main/java/com/example/movie_api/MovieController.java
@@ -0,0 +1,30 @@
+package com.example.movie_api;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/movies")
+
+public class MovieController {
+
+    @Autowired
+    private MovieService movieService;
+
+    @GetMapping
+    public List<Movie> getAllMovies(){
+        return movieService.getAllMovies();
+    }
+
+    @PostMapping
+    public  Movie addMovie(@RequestParam String title, @RequestParam String status){
+        return movieService.addMovie(title, status);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteMovie(@PathVariable Long Id){
+        movieService.deleteMovie(Id);
+    }
+}
