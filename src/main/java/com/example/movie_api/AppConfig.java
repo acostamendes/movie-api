e3244b0016daa0a3d7f83fb58d6f59ecc1a49961
André Mendes Costa <andre.mendes.sp01@gmail.com>
2024-09-06 10:48:11 -0300

    AppConfig - to solve a bug with RestTemplate

---
+++ src/main/java/com/example/movie_api/AppConfig.java
@@ -0,0 +1,14 @@
+package com.example.movie_api;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+}
